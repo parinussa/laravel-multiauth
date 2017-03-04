e7344f65d87e0d3212d1ca7964ac79aa4009382b
Noval Parinussa <novalparinussa@gmail.com>
2017-03-05 06:29:09 +0700

    Add login & showLoginForm method in admin login controller

--- app/Http/Controllers/Auth/AdminLoginController.php
+++ app/Http/Controllers/Auth/AdminLoginController.php
@@ -2,13 +2,43 @@
 
 namespace App\Http\Controllers\Auth;
 
-use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Auth;
 
 class AdminLoginController extends Controller
 {
+	public function __construct()
+	{
+		$this->middleware('guest:admin');
+	}
+
 	public function showLoginForm()
 	{
 		return view('auth.admin-login');
+	}
+
+	public function login(Request $request)
+	{
+		$credentials = [
+			'email' 	=> request('email'),
+			'password' 	=> request('password')
+		];
+
+		// Validate the form data
+		$this->validate($request, [
+			'email' 	=> 'required|email',
+			'password' 	=> 'required|min:6'
+		]);
+
+		// Attempt to log user in
+		if (auth()->guard('admin')->attempt($credentials, $request->remember)) {
+			// If successful, then redirect to their intended location
+			return redirect()->intended(route('admin.dashboard'));
+		}
+
+		// If unsuccessful, then redirect back to the login with the form data
+		return redirect()->back()->withInput(request()->only('email', 'remember'));
+
 	}
 }
